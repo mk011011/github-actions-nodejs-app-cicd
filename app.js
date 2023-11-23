73d2be6cb332e8b1605adf3c88de7558349eafd6
mk011011 <144010941+mk011011@users.noreply.github.com>
2023-11-23 22:43:05 +0900

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Node.js 배포!\n');
+  res.end('Node.js distribution!\n');
 });
 
 server.listen(port, hostname, () => {
